33e4bc110319fc9a85b5645cb7b638e5f5ad83fa
ROBOTICSENGINEER <mohsen.jafarzade@gmail.com>
2020-12-10 12:11:35 -0700

    Update efficient_b3_fp32.py

--- train/efficient_b3_fp32.py
+++ train/efficient_b3_fp32.py
@@ -9,7 +9,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import time
 
-from timm.models.efficientnet import efficientnet_b3  # timm library
+from ..timm.models.efficientnet import efficientnet_b3  # timm library
 
 
 n_cpu = 32
@@ -85,7 +85,7 @@ def accuracy(output, target, topk=(1,)):
 class known_train_data_class(Dataset):
 
   def __init__(self, transform=None):
-    with open('./data/imagenet_1000_train.csv') as f:
+    with open('../data/imagenet_1000_train.csv') as f:
       self.samples = [line.rstrip() for line in f if line is not '']
     self.transform = transform
 
@@ -106,7 +106,7 @@ class known_train_data_class(Dataset):
 class known_val_data_class(Dataset):
 
   def __init__(self, transform=None):
-    with open('./data/imagenet_1000_val.csv') as f:
+    with open('../data/imagenet_1000_val.csv') as f:
       self.samples = [line.rstrip() for line in f if line is not '']
     self.transform = transform
 
